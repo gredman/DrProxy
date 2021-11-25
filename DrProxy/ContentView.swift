740132dc83871119c89a9f01d53358a5143e46ee
Gareth Redman <GarethRedman@co-operativebank.co.nz>
2021-11-26 11:39:25 +1300

    Initial Commit
    Initial Commit

---
+++ DrProxy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrProxy
+//
+//  Created by Gareth Redman on 26/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DrProxy/DrProxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrProxyApp.swift
+//  DrProxy
+//
+//  Created by Gareth Redman on 26/11/21.
+//
+
+import SwiftUI
+
+@main
+struct DrProxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
